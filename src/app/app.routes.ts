9c84dab2f0f5942d1760a7ec59fa501bd30d2e5e
joaopaulocjr <joaopaulocruzjr@gmail.com>
2026-09-23 23:42:19 -0300

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () =>
+            import('./pages/auth/auth').then((m) => m.Auth),
+    },
+    {
+        path: 'home',
+        loadComponent: () =>
+            import('./pages/home/home').then((m) => m.Home),
+    },
+    
+];
